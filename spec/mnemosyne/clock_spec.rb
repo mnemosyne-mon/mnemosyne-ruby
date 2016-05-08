6ca51c84e8c53cf75ff826277c4b4870b55d447e
Jan Graichen <jg@altimos.de>
2016-05-08 13:39:08 +0200

    Add Clock.to_tick spec

--- spec/mnemosyne/clock_spec.rb
+++ spec/mnemosyne/clock_spec.rb
@@ -4,8 +4,17 @@ describe Mnemosyne::Clock do
   describe '.tick' do
     it 'returns nsec timestamp' do
       Timecop.freeze do
-        expect(Mnemosyne::Clock.tick).to eq(Time.now.to_i * 1_000_000_000 + Time.now.nsec)
+        expect(Mnemosyne::Clock.tick)
+          .to eq(Time.now.to_i * 1_000_000_000 + Time.now.nsec)
       end
+    end
+  end
+
+  describe '.to_tick' do
+    it 'returns nsec timestamp' do
+      time = Time.now
+      expect(Mnemosyne::Clock.to_tick(time))
+        .to eq(time.to_i * 1_000_000_000 + time.nsec)
     end
   end
 end
