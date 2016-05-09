9989390fb163e7b8998d96f01d1bbd7ee307306c
Jan Graichen <jg@altimos.de>
2016-05-09 21:37:23 +0200

    Move AC process action probe

---
+++ lib/mnemosyne/probes/action_controller/process_action.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Mnemosyne
+  module Probes
+    module ActionController
+      module ProcessAction
+        class Probe < ::Mnemosyne::Probe
+          subscribe 'process_action.action_controller'
+
+          def call(trace, name, start, finish, id, payload)
+            start  = ::Mnemosyne::Clock.to_tick(start)
+            finish = ::Mnemosyne::Clock.to_tick(finish)
+
+            meta = {
+              controller: payload[:controller],
+              action: payload[:action],
+              format: payload[:format]
+            }
+
+            span = ::Mnemosyne::Span.new "rails.#{name}",
+              start: start, finish: finish, meta: meta
+
+            trace << span
+          end
+        end
+      end
+    end
+
+    register('ActionController::Base', 'action_controller', ActionController::ProcessAction::Probe.new)
+  end
+end
