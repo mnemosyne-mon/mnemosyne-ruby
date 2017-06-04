f602ec83a8b46dcb830d636c7ea19ac85d784c12
Jan Graichen <jgraichen@altimos.de>
2017-06-04 19:48:44 +0200

    Move restify instrumentation to probe

--- lib/mnemosyne/probes/restify/base.rb
+++ lib/mnemosyne/probes/restify/base.rb
@@ -6,9 +6,32 @@ module Mnemosyne
       module Base
         class Probe < ::Mnemosyne::Probe
           def setup
-            require 'mnemosyne/middleware/restify'
+            ::Restify::Adapter::Base.prepend Instrumentation
+          end
 
-            ::Restify::Adapter::Base.prepend ::Mnemosyne::Middleware::Restify
+          module Instrumentation
+            def call(request)
+              if (trace = ::Mnemosyne::Instrumenter.current_trace)
+                meta = {url: request.uri.to_s, method: request.method}
+
+                span = ::Mnemosyne::Span.new 'external.http.restify', \
+                  meta: meta
+
+                span.start!
+
+                request.headers['X-Mnemosyne-Transaction'] = trace.transaction
+                request.headers['X-Mnemosyne-Origin'] = span.uuid
+
+                super.tap do |x|
+                  x.add_observer do |_, _response, _err|
+                    span.finish!
+                    trace << span
+                  end
+                end
+              else
+                super
+              end
+            end
           end
         end
       end
