ec48390a860ffc3cd63e2512c92a6d3289c1bd8a
Franz Liedke <franz@develophp.org>
2021-08-23 16:05:18 +0200

    Remove incorrect context in spec

--- spec/mnemosyne/probes/active_record/query_spec.rb
+++ spec/mnemosyne/probes/active_record/query_spec.rb
@@ -6,40 +6,38 @@ require 'support/active_record'
 RSpec.describe ::Mnemosyne::Probes::ActiveRecord::Query, probe: :rails do
   before { Record.create name: 'test' }
 
-  context 'adapter: sidekiq' do
-    it 'creates a trace' do
-      trace = with_trace do
-        Record.where(name: 'test').take
-      end
+  it 'creates a trace' do
+    trace = with_trace do
+      Record.where(name: 'test').take
+    end
 
-      expect(trace.span.size).to eq 2
+    expect(trace.span.size).to eq 2
 
-      trace.span.first.tap do |span|
-        expect(span.name).to eq 'db.query.active_record'
-        expect(span.meta.keys).to match_array %i[sql binds]
+    trace.span.first.tap do |span|
+      expect(span.name).to eq 'db.query.active_record'
+      expect(span.meta.keys).to match_array %i[sql binds]
 
-        if ActiveRecord::VERSION::MAJOR >= 6
-          expect(span.meta).to match \
-            sql: 'SELECT "records".* FROM "records" WHERE "records"."name" = ? LIMIT ?',
-            binds: ['test', 1]
-        elsif ActiveRecord::VERSION::MAJOR >= 5
-          expect(span.meta).to match \
-            sql: 'SELECT  "records".* FROM "records" WHERE "records"."name" = ? LIMIT ?',
-            binds: ['test', 1]
-        else
-          expect(span.meta).to match \
-            sql: 'SELECT  "records".* FROM "records" WHERE "records"."name" = ? LIMIT 1',
-            binds: ['test']
-        end
-      end
-
-      trace.span[1].tap do |span|
-        expect(span.name).to eq 'db.instantiation.active_record'
-        expect(span.meta.keys).to match_array %i[class_name count]
+      if ActiveRecord::VERSION::MAJOR >= 6
         expect(span.meta).to match \
-          class_name: 'Record',
-          count: 1
+          sql: 'SELECT "records".* FROM "records" WHERE "records"."name" = ? LIMIT ?',
+          binds: ['test', 1]
+      elsif ActiveRecord::VERSION::MAJOR >= 5
+        expect(span.meta).to match \
+          sql: 'SELECT  "records".* FROM "records" WHERE "records"."name" = ? LIMIT ?',
+          binds: ['test', 1]
+      else
+        expect(span.meta).to match \
+          sql: 'SELECT  "records".* FROM "records" WHERE "records"."name" = ? LIMIT 1',
+          binds: ['test']
       end
+    end
+
+    trace.span[1].tap do |span|
+      expect(span.name).to eq 'db.instantiation.active_record'
+      expect(span.meta.keys).to match_array %i[class_name count]
+      expect(span.meta).to match \
+        class_name: 'Record',
+        count: 1
     end
   end
 end
